97955a0cf8d4609898a881afc15da1c18672f3b2
Ryo ICHIKAWA <ryo.ichikawa.0308@gmail.com>
2025-11-10 17:15:40 +0000

    コメント追加

--- scms-backend/src/types/express.d.ts
+++ scms-backend/src/types/express.d.ts
@@ -1,8 +1,8 @@
 import { JwtPayload } from 'src/service/auth/jwt.strategy';
-
 // ExpressのRequestオブジェクトを拡張し、認証情報を参照できるようにする。
 declare global {
   declare namespace Express {
+    // JWTペイロードと同一型として扱う
     // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     interface User extends JwtPayload {}
   }
