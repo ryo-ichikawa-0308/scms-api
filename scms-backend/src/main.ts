98f2d3bf8f65acea4d5120fa22e89821c6305171
Ryo ICHIKAWA <ryo.ichikawa.0308@gmail.com>
2025-11-19 14:33:49 +0000

    画面サーバーからのアクセスを受け入れる設定

--- scms-backend/src/main.ts
+++ scms-backend/src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+  app.enableCors({
+    origin: 'http://frontend:4200',
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
